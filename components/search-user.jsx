7bb6d9c205ae46314fc47b737652e4f91eded1af
German Bertea <german.bertea@altran.com>
2021-02-21 18:12:54 +0100

    Add funct

--- components/search-user.jsx
+++ components/search-user.jsx
@@ -2,14 +2,21 @@ import React from "react";
 
 function SearchUser({ value, setValue, onSearch }) {
     return (
-        <div>
-            <input
-                type="text"
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-            />
-            <button onClick={() => onSearch()}>Ir a</button>
-        </div>
+        <div className="input-group">
+            <div className="form-outline">
+                <input className="mr-sm-2"
+                    placeholder="Search"
+                    type="text" // value={value}
+                    onChange={(e) => setValue(e.target.value)} ></input>
+                <button className="btn btn-outline-success my-2 my-sm-0" onClick={() => onSearch()} >Ir a</button>
+            </div>
+
+            <style jsx>{`
+                       .input-group {
+                            width: 100%;
+                        }
+                        `}</style>
+        </div >
     );
 }
 
--- components/user-card-list.jsx
+++ components/user-card-list.jsx
@@ -4,17 +4,29 @@ import UserCard from "./user-card";
 
 function UserCardList({ users }) {
     return (
-        <ul>
-            {users.map((user) => (
-                <li key={user.id}>
-                    <Link href={`/users/${user.login}`}>
-                        <a>
-                            <UserCard name={user.login} />
-                        </a>
-                    </Link>
-                </li>
-            ))}
-        </ul>
+        <div>
+
+            <ul className="list-group">
+                {users.map((user) => (
+                    <li key={user.id} className="list-group-item">
+                        <Link href={`/users/${user.login}`}>
+                            <a>
+                                <UserCard name={user.login} />
+                            </a>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            <style jsx>{`
+                        .list-group {
+                            max-height: 500px;
+                            margin-bottom: 10px;
+                            overflow:scroll;
+                            -webkit-overflow-scrolling: touch;
+                        }
+                        `}</style>
+        </div>
+
     );
 }
 
