a617c43fe5d69d0b4e90e6bf0cb56625442837b4
German Bertea <german.bertea@altran.com>
2021-02-22 16:21:17 +0100

    Cmabios view

--- components/user-card-list.jsx
+++ components/user-card-list.jsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import UserCard from "./user-card";
 
 function UserCardList({ users }) {
+
+    console.log(users)
     return (
         <div>
             <ul className="list-group">
@@ -10,20 +12,20 @@ function UserCardList({ users }) {
                     <li key={user.id} className="list-group-item">
                         <Link href={`/users/${user.login}`}>
                             <a>
-                                <UserCard name={user.login} />
+                                <UserCard name={user.login} image={user.avatar_url} />
                             </a>
                         </Link>
                     </li>
                 ))}
             </ul>
-            <style jsx>{`
+            {/* <style jsx>{`
                         .list-group {
                             max-height: 500px;
                             margin-bottom: 10px;
                             overflow:scroll;
                             -webkit-overflow-scrolling: touch;
                         }
-                        `}</style>
+                        `}</style> */}
         </div>
 
     );
--- components/user-card.jsx
+++ components/user-card.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import Image from 'next/image';
 
-function UserCard({ name }) {
+function UserCard({ name, image }) {
     return (
-        <div>
-            <h1>{name}</h1>
+        // <div>
+        //     <h1>{name}</h1>
+        // </div>
+        <div className="card"
+            style={{ width: "24rem", position: "center" }}>
+            <div className="card-header">
+                <h5 className="card-title">User: {name}</h5>
+            </div>
+            <Image src={image}
+                width={70}
+                height={150}
+                className="card-img-top"
+                objectFit="fit"
+                objectPosition="center"></Image>
+            <div className="card-body">
+                {/* <h5 className="card-title">Nombre: {user.name}</h5> */}
+
+                {/* <a href={user.html_url} className="btn btn-primary">Go {user.login} GithubPage</a> */}
+            </div>
         </div>
     );
 }
